7c5c908f0ed808dc7546c3700b4cadce93d9def3
DiegoAlvarez <diego13alvarez2004@gmail.com>
2025-05-22 20:58:39 +0000

    segundo avance de codigo

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -16,3 +16,30 @@ def saldo_vacaciones_empleado(codigo_empleado):
         return f"Saldo actual de vacaciones para {empleados[codigo_empleado]['nombre']}: {empleados[codigo_empleado]['saldo_vacaciones']} días."
     else:
         return "Empleado no encontrado."
+# Función para solicitar vacaciones
+def solicitar_vacaciones(codigo_empleado, dias_solicitados):
+    if codigo_empleado in empleados:
+        saldo_actual = empleados[codigo_empleado]['saldo_vacaciones']
+
+        if saldo_actual >= dias_solicitados:
+            empleados[codigo_empleado]['saldo_vacaciones'] -= dias_solicitados
+            return f"Solicitud de vacaciones aprobada para {empleados[codigo_empleado]['nombre']}. Saldo restante: {empleados[codigo_empleado]['saldo_vacaciones']} días."
+        else:
+            return f"No hay suficientes días de vacaciones disponibles para {empleados[codigo_empleado]['nombre']}."
+    else:
+        return "Empleado no encontrado."
+
+# Función para agregar un nuevo empleado
+def agregar_empleado(codigo_empleado, nombre, saldo_vacaciones, contraseña):
+    if contraseña == contraseña_admin:
+        if codigo_empleado not in empleados:
+            empleados[codigo_empleado] = {'nombre': nombre, 'saldo_vacaciones': saldo_vacaciones}
+            return f"Nuevo empleado {nombre} agregado con éxito."
+        else:
+            return "El empleado ya existe. Utilice otro código."
+    else:
+        return "Contraseña incorrecta. No tiene permisos para agregar nuevos empleados."
+
+# Bienvenida e inicio de sesión
+print("Bienvenido al Sistema de Gestión de Vacaciones de Vendomática")
+print("-------------------------------------------------------------")
